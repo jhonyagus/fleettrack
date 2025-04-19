fae9ab462a3bd3f19424eac266cc87ad7a7a1280
Jhony Naranjo <jhonyagus@hotmail.com>
2025-04-18 23:15:09 -0500

    initial commit

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <p className="read-the-docs text-cyan-300 text-2xl text-right">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
