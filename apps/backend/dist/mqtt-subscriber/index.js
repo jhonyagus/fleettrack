146513505248e8a3d90a20f03c4915d2e45767ca
Jhony Naranjo <jhonyagus@hotmail.com>
2025-04-28 08:04:33 -0500

    change to use tsx

--- apps/backend/dist/mqtt-subscriber/index.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-import { subscribeToMqttTopic } from './services/mqtt-client.service';
-import { handleGpsData } from './services/gps-data.service';
-dotenv.config();
-subscribeToMqttTopic(process.env.MQTT_TOPIC, handleGpsData);
